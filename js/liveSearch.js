a122ac69e61fb3aa3eafc7a8b8c2d87f55e572fd
Shahbaaz Khan <75750030+ShahbaazK@users.noreply.github.com>
2020-12-10 00:13:43 +0000

    Add files via upload

---
+++ js/liveSearch.js
@@ -0,0 +1,32 @@
+function search(string) {
+
+    //XML HTTP request here (W3 schools)
+
+    //Json.parse(this.responseText)
+
+    //call renderJsonData()
+
+
+    //request open
+    //request send
+
+}
+
+
+
+function renderJsonData(Json) {
+
+    //Java script for each loop to loop through all json data
+
+    //Json.forEach
+
+
+    //access object properties via => obj.NameInDataBase
+
+    //write to html
+
+
+}
+
+//text search with a button that will direct you to the trainers page.
+//a button that will link back again
